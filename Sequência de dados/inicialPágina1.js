3626974d1d37ca78d9e81e9b9b15f3a3a3c320cc
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-01-29 17:40:25 -0300

    7

---
+++ Sequência de dados/inicialPágina1.js
@@ -0,0 +1,29 @@
+
+/*Declaração das váriaveis.*/
+var valor=2800;
+var i;
+
+/*Validação de dado de i até 1*/
+for(var i=0;i<1;i++){
+
+/*Validação de dado se é menor,maior caso contrário vai ser diferente igual.*/
+ if(valor<=2700){
+
+
+/*Imprimir a mesagem para classificar sobre o valor menor igual.*/
+document.write("O valor é de menor e igual=\n"+valor);
+
+ }if(valor>=2700){
+
+/*Imprimir a mesagem para classificar sobre o valor maior igual.*/
+document.write("O valor é de maior e igual=\n"+valor);
+
+
+ }else if(valor!=2700){
+
+/*Imprimir a mesagem para classificar sobre o valor diferente igual.*/
+document.write("O valor é de diferente igual=\n"+valor);
+
+ }
+
+}
